ab758f8d3f633da5a1e1dfeca701758e264e0b8d
Bhavya <bhavya.2k03@gmail.com>
2025-02-06 22:26:06 +0530

    Add files via upload

---
+++ 957_B.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        vector<int> vec(k);
+        int temp=0;
+        for(int i=0; i<k; i++) {cin>>vec[i]; temp=max(temp,vec[i]);}
+
+        int flag=1, sum1=0, sum2=0;
+        for(auto &i:vec)
+        {
+            if(i!=temp || flag==0)
+            {
+                sum1+=i-1;
+                sum2+=i;
+            }
+            else flag=0;
+        }
+        cout<<sum1+sum2<<endl;
+
+    }
+
+
+
+
+
+
+
+
+
+return 0;
+}
